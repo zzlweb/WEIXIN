3875ff1e0bc68f6d7ad76ede67a4f6cf67a7948a
zhengliang.zhang <zhengliang.zhang@united-imaging.com>
2021-12-30 15:27:51 +0800

    增加我的借阅页面

---
+++ cloudfunctions/cancelBook/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+    try {
+        return await db.collection('books').where({
+            _id: event._id
+        }).update({
+            data: {
+              status: false, 
+              openid: '', 
+            },
+          })
+    } catch (error) {
+       return error
+    }
+}
--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -1,19 +1,88 @@
 // miniprogram/pages/detail/index.js
+const app = getApp();
+const db = wx.cloud.database();
+wx.cloud.init();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 借阅的书籍
+    borrow_List: [],
+    // 是否显示空状态
+    isEmpty: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.queryBorrowBook()
   },
+
+  /**
+   * 根据appid查询借阅书籍
+   */
+
+  async queryBorrowBook() {
+    try {
+      const list = await db.collection('books').where({
+        openid: app.openid
+      }).get()
+
+      if (!list.data || list.data.length === 0) {
+        this.setData({
+          isEmpty: true, 
+          borrow_List: []
+        })
+      } else {
+        this.setData({
+          borrow_List: list.data
+        })
+      }
+    } catch (error) {
+      wx.showToast({
+        title: '查询数据失败',
+        icon: 'none',
+        duration: 2000,
+      });
+    }
+  },
+
+  /**
+   * 取消个人收藏书籍
+   * @param {*} 
+   */
+  delCollection(e){
+    wx.cloud.callFunction({
+      name: 'cancelBook', 
+      data: {
+        _id: `${e.currentTarget.dataset.id}`
+      }
+    }).then(() => {
+      this.queryBorrowBook()
+      wx.showToast({
+        title: '归换成功',
+        icon: 'none',
+        duration: 2000,
+      });
+    })
+  }, 
+
+  /**
+   * 返回首页
+   */
+
+   goPage(e) {
+    e.currentTarget.dataset.src ? wx.switchTab({
+          url: e.currentTarget.dataset.src
+    }) : wx.showToast({
+          title: '暂未开放',
+          icon: 'none', 
+          duration: 2000,
+    })
+},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
