2ebe09915438a0fe5b98b2e60adaf5e88624ff3e
zhengliang.zhang <zhengliang.zhang@united-imaging.com>
2022-01-04 11:47:24 +0800

    完成归还书籍页面

--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -11,6 +11,8 @@ Page({
     borrow_List: [],
     // 是否显示空状态
     isEmpty: false,
+    // 禁用按钮状态
+    disabled: true 
   },
 
   /**
@@ -23,7 +25,6 @@ Page({
   /**
    * 根据appid查询借阅书籍
    */
-
   async queryBorrowBook() {
     try {
       const list = await db
@@ -37,10 +38,12 @@ Page({
         this.setData({
           isEmpty: true,
           borrow_List: [],
+          disabled:true
         });
       } else {
         this.setData({
           borrow_List: list.data,
+          disabled: false
         });
       }
     } catch (error) {
@@ -56,41 +59,94 @@ Page({
    * 取消个人收藏书籍
    * @param {*}
    */
-  delCollection(e) {
-    wx.cloud
-      .callFunction({
-        name: 'cancelBook',
-        data: {
-          _id: `${e.currentTarget.dataset.id}`,
-        },
-      })
-      .then(() => {
-        this.queryBorrowBook();
-        wx.showToast({
-          title: '归换成功',
-          icon: 'none',
-          duration: 2000,
-        });
+  async delCollection(e) {
+    wx.showLoading({
+      title: '归还中',
+    });
+
+    try {
+      await wx.cloud
+        .callFunction({
+          name: 'cancelBook',
+          data: {
+            _id: `${e.currentTarget.dataset.id}`,
+          },
+        })
+      wx.hideLoading();
+      wx.showToast({
+        title: '归还成功',
+        icon: 'none',
+        duration: 2000,
       });
+    } catch (error) {
+      wx.hideLoading();
+      wx.showToast({
+        title: '归还失败',
+        icon: 'none',
+        duration: 2000,
+      })
+    }
+
+    await this.queryBorrowBook();
+
   },
 
   /**
    * 返回首页
    */
-
   goPage(e) {
-    e.currentTarget.dataset.src
-      ? wx.switchTab({
-          url: e.currentTarget.dataset.src,
-        })
-      : wx.showToast({
-          title: '暂未开放',
+    e.currentTarget.dataset.src ?
+      wx.switchTab({
+        url: e.currentTarget.dataset.src,
+      }) :
+      wx.showToast({
+        title: '暂未开放',
+        icon: 'none',
+        duration: 2000,
+      });
+  },
+
+  /**
+   * 归还全部书籍
+   */
+  async onClickButton(e) {
+    const IdArray = [] 
+    this.data.borrow_List.forEach( item => {
+      IdArray.push(item._id)
+    })
+
+    for (let index = 0; index < IdArray.length; index++) {
+      try {
+        await wx.cloud
+          .callFunction({
+            name: 'cancelBook',
+            data: {
+              _id: index,
+            },
+          })
+        wx.hideLoading();
+        wx.showToast({
+          title: '归还成功',
           icon: 'none',
           duration: 2000,
         });
-  },
 
-  onClickButton() {},
+        this.setData({
+          isEmpty: true,
+          borrow_List: [],
+          disabled:true
+        })
+
+      } catch (error) {
+        wx.hideLoading();
+        wx.showToast({
+          title: '归还失败',
+          icon: 'none',
+          duration: 2000,
+        })
+      }
+    }
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
